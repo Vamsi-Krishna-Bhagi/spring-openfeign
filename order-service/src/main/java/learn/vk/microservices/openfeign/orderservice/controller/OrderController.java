8f5ea9e98f2b281fbbe5ffca73adc3e749f3bfaf
Vamsi <bhagivamsi@gmail.com>
2023-11-12 13:53:45 -0500

    Added OpenApi and Swagger

--- order-service/src/main/java/learn/vk/microservices/openfeign/orderservice/controller/OrderController.java
+++ order-service/src/main/java/learn/vk/microservices/openfeign/orderservice/controller/OrderController.java
@@ -1,5 +1,11 @@
 package learn.vk.microservices.openfeign.orderservice.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Schema;
 import learn.vk.microservices.openfeign.orderservice.dto.Message;
 import learn.vk.microservices.openfeign.orderservice.dto.OrderDto;
 import learn.vk.microservices.openfeign.orderservice.service.OrderService;
@@ -9,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
+@OpenAPIDefinition(info = @Info(title = "Order Api"))
 public class OrderController {
     private final OrderService orderService;
 
@@ -17,11 +24,14 @@ public class OrderController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all orders")
     public List<OrderDto> getOrders() {
         return orderService.getOrders();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get order by id")
+    @Parameters(@Parameter(name = "id", description = "order id"))
     public OrderDto getOrder(@PathVariable Long id) {
         return orderService.getOrder(id);
     }
